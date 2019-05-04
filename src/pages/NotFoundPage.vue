9aea604690f63eda68d117d080d7ffd9703367a6
Damian Nowak <damiannowak@nurtureagency.com>
2019-05-04 10:51:33 +0200

    some changes in routing

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -42,6 +42,7 @@
         <div class="copyright">
           © {{year}} utworzone z <i class="tim-icons icon-heart-2"></i> przez
           <a href="https://dnowak.dev" target="_blank" rel="noopener">Damian Nowak</a>
+          <span v-if="version">{{versionText}}</span>
         </div>
       </div>
     </footer>
@@ -49,11 +50,20 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      year: new Date().getFullYear()
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear(),
+        version: false,
+        versionText: ''
+      }
+    },
+    mounted() {
+      if (typeof process.env.VUE_APP_HEROKU_SLUG_COMMIT !== "undefined"
+              && typeof process.env.VUE_APP_HEROKU_RELEASE_VERSION !== "undefined") {
+        this.version = true;
+        this.versionText =  ` | ${process.env.VUE_APP_HEROKU_SLUG_COMMIT}${process.env.VUE_APP_HEROKU_RELEASE_VERSION}`;
+      }
     }
-  }
-};
+  };
 </script>
